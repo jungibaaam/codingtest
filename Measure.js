da2789f9eb2be649994c65787a0c32b206ce24d8
jungi <joongi1217@gmail.com>
2022-11-22 15:03:11 +0900

    Measure

---
+++ Measure.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(i = 0; i < n; i++){
+        if(n % i === 0){
+            answer += i;
+        }
+    }
+    answer += n;
+    return answer;
+}
